89eda9bce4830e430521547d3c9e24d5845a9385
IsMoreiraKt <ismaelmoreiraubuntu@gmail.com>
2024-11-04 19:38:07 +0000

    feat: Update the main component to use a variety of themes.

--- lda_frontend/src/App.vue
+++ lda_frontend/src/App.vue
@@ -1,26 +1,41 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template :class="['app-container', isDark ? 'bg-dark text-white' : 'bg-light']">
+  <div 
+    id="app-wrapper"
+    :class="['app-container', isDark ? 'bg-dark text-white' : 'bg-light']"
+  >
+    <h1>Hello World</h1>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+<script>
+  import { mapState, mapActions } from 'vuex';
+
+  export default {
+    name: 'App',
+    components: { },
+    computed: {
+      ...mapState(['isDark'])
+    },
+    methods: {
+      ...mapActions(['toggleTheme'])
+    }
   }
-}
 </script>
 
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+
+    box-sizing: border-box;
+  }
+
+  .app-container {
+    width: 100vw;
+    height: 100vh;
+  }
 </style>
