f74b393a81ab3467652e3a8224cd04c320d7371b
IsMoreiraKt <ismaelmoreiraubuntu@gmail.com>
2024-11-04 18:04:47 +0000

    feat: Adding an api health check route.

---
+++ lda_server/routes/health.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, jsonify
+
+
+
+health = Blueprint('health', __name__)
+
+
+
+@health.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({'status': 'healthy'}), 200
